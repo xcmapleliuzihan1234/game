8fd6cee896a29da496629bbdca244631fef5cb47
xcmapleliuzihan1234 <1654985534@qq.com>
2020-07-04 17:01:40 +0800

    修改了注册页面的代码，在src下建立了java使点击注册时跳转页面

---
+++ src/mybean/data/Register.java
@@ -0,0 +1,52 @@
+package mybean.data;
+
+public class Register {
+    String logname = "";
+    String phone = "";
+    String address = "";
+    String realname = "";
+    String backNews = "«Î ‰»Î–≈œ¢";
+
+    public Register() {
+    }
+
+    public void setLogname(String logname) {
+        this.logname = logname;
+    }
+
+    public String getLogname() {
+        return this.logname;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getRealname() {
+        return this.realname;
+    }
+
+    public void setBackNews(String backNews) {
+        this.backNews = backNews;
+    }
+
+    public String getBackNews() {
+        return this.backNews;
+    }
+}
